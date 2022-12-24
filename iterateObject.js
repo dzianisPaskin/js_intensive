5cd265686d1d4ea4a312b89edcf1467ebcf37c2a
Dzianis <paskin.denis.00gmail.com>
2022-12-24 15:41:13 +0100

    added task03

---
+++ iterateObject.js
@@ -0,0 +1,28 @@
+let myIterable = {
+  from: 1,
+  to: 4,
+  [Symbol.iterator]:  function() {
+
+    if(this.from > this.to) {
+      throw new Error('the value of from must be less than to')
+    } else if(!myIterable.hasOwnProperty('from') || !myIterable.hasOwnProperty('to')) {
+     throw new Error('Specify the properties of the object "from" and "to".')
+    } else if(typeof this.from !== 'number' || typeof this.to !== 'number') {
+      console.log(this.from)
+      throw new Error('The values must be the numbers!')
+    }
+    return {
+      
+      current: this.from,
+      last: this.to,
+      
+      next() {   
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ }
+        } else {
+          return { done: true }
+        }
+      }
+    }
+  }
+}
