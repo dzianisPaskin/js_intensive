ac0f4cbe218affb14874a3f54997550efd37ff13
Dzianis <paskin.denis.00gmail.com>
2022-12-29 16:23:37 +0100

    modified the stack

--- stackImpl.js
+++ stackImpl.js
@@ -8,4 +8,16 @@ class Stack {
   pop() {
     this.stack.pop();
   }
+  peek() {
+    if(this.stack.length == 0) {
+      return null
+    }
+    return this.items[this.items.lenght - 1]
+  }
+  getSize() {
+    return this.items.length
+  }
+  isEmpty() {
+    return this.getSize() === 0
+  }
  }
